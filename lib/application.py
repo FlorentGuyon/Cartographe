1600ebb1f83eb04efa11a2bbacf6f800e1c30c1f
Florent Guyon <florent.guyon@protonmail.com>
2020-12-16 23:15:33 +0100

    Add the Vitals page
    Add a new layout with current Battery level, CPU, RAM and Disk usage

--- lib/application.py
+++ lib/application.py
@@ -2,6 +2,9 @@
 
 from . import config
 import os
+import psutil
+import datetime
+import pathlib
 import plotly.graph_objects as go
 from dash import Dash
 from dash.dependencies import Output, Input
@@ -191,25 +194,118 @@ class MapChart():
         return dlx.geojson_to_geobuf(dlx.dicts_to_geojson(self.points))
 
 
+class Indicator():
+
+    id = None
+    title = None
+    subtitle = None
+    min = None
+    max = None
+    value = None
+    suffix = None
+    reference = None
+    threshold = None
+    mode = None
+    layout = None
+
+    def __init__(self, id, title=None, subtitle=None, min=0, max=100, value=0, suffix="%", reference=50, threshold=0, mode="gauge+number+delta"):
+        """Define the layout of the chart"""
+        self.id = id
+        self.title = title
+        self.subtitle = subtitle
+        self.min = min
+        self.max = max
+        self.value = value
+        self.suffix = suffix
+        self.reference = reference
+        self.threshold = threshold
+        self.mode = mode
+        self.layout = dcc.Graph(id=self.id, figure=self.get_layout())
+
+    def get_layout(self):
+        """Get the updated layout of the indicator"""
+        title = None
+        if self.title:
+            title = {"text": f"{self.title}"}
+        if self.subtitle:
+            title["text"] += f"<br><span style='font-size:0.8em;color:gray'>{self.subtitle}</span>"
+
+        figure = go.Figure(
+            go.Indicator(
+                title=title,
+                mode=self.mode,
+                value=self.value,
+                number={'suffix': self.suffix},
+                domain={'x': [0, 1], 'y': [0, 1]},
+                delta={'reference': self.reference},
+                gauge={
+                    'borderwidth': 0,
+                    'axis': {'range': [self.min, self.max], 'visible': False, 'tickcolor': "#dbd6d6"},
+                    'bar': {'color': "#4a98c9", 'thickness': 0.05},
+                    'threshold': {
+                        'line': {'color': "red", 'width': 0.5},
+                        'thickness': 1,
+                        'value': self.threshold}
+                }
+            )
+        )
+
+        figure.update_layout(
+            paper_bgcolor="#181a1b",
+            font={'color': "#dbd6d6"}
+        )
+
+        return figure
+
+
 dash = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP], update_title=None)
+layouts = {}
+default_layout = "dashboard"
+current_layout = default_layout
 network_chart = NetworkChart()
 map_chart = MapChart()
+battery_status = Indicator("battery_status", mode="number", title="Battery")
+ram_status = Indicator("ram_status", mode="number", title="RAM")
+cpu_status = Indicator("cpu_status", mode="number", title="CPU")
+disk_status = Indicator("disk_status", mode="number", title="Disk")
+
+
+def bytes2human(n, decimals=0):
+        # http://code.activestate.com/recipes/578019
+    symbols = ('K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y')
+    prefix = {}
+    for i, s in enumerate(symbols):
+        prefix[s] = 1 << (i + 1) * 10
+    for s in reversed(symbols):
+        if n >= prefix[s]:
+            value = float(n) / prefix[s]
+            return f'%.{decimals}f%s' % (value, s)
+    return "%sB" % n
+
+
+def get_navlinks():
+    """Return the list of links for the navbar"""
+    return [
+        dbc.NavItem(dbc.NavLink(
+            [html.Img(src=f'{config.get_config().application.icons_path}dashboard.svg', className="feather"), " Dashboard"],
+            href="/dashboard",
+            className=("active" if current_layout == "dashboard" else ""))),
+        dbc.NavItem(dbc.NavLink(
+            [html.Img(src=f'{config.get_config().application.icons_path}vitals.svg', className="feather"), " Vitals"],
+            href="/vitals",
+            className=("active" if current_layout == "vitals" else "")))
+    ]
 
 
 class Application(Thread):
 
     running = None
-    queue = None
 
     def __init__(self, queue):
         """Generate a ready to run WEB server hosting a custom web site"""
         Thread.__init__(self, daemon=True)
         self.queue = queue
         self.running = False
-
-        charts_update_clock = dcc.Interval(
-            id='charts_update_clock',
-            interval=config.get_config().application.components.update_interval)
 
         ##########################################################################
         #                            L  A  Y  O  U  T                            #
@@ -238,7 +334,7 @@ class Application(Thread):
                         dbc.NavbarBrand(config.get_config().application.title,
                                         className="ml-2")
                     ],
-                    href="#"
+                    href=f'/{default_layout}'
                 ),
                 dbc.NavbarToggler(id="navbar-toggler"),
             ],
@@ -266,11 +362,9 @@ class Application(Thread):
                 html.Div(
                     [
                         dbc.Nav(
-                            [
-                                dbc.NavItem(dbc.NavLink([html.Img(
-                                    src=f'{config.get_config().application.icons_path}dashboard.png', className="feather"), " Dashboard"], href="#", active=True))
-                            ],
-                            className="flex-column"
+                            children=get_navlinks(),
+                            className="flex-column",
+                            id="nav_links",
                         )
                     ],
                     className="sidebar-sticky pt-3")
@@ -293,17 +387,44 @@ class Application(Thread):
         #       |         **************************************|
         #       -------------------------------------------------
         #
-        main = html.Main(
-            [
-                charts_update_clock,
-                html.Div(html.H2(
-                    "Network"), className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom"),
-                network_chart.layout,
-                html.Div(html.H2("Map"), id="geo",
-                         className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom"),
-                map_chart.layout
+        dashboard_update_clock = dcc.Interval(
+            id='dashboard_update_clock',
+            interval=config.get_config().application.components.dashboard_update_interval)
 
-            ],
+        layouts["dashboard"] = [
+            dashboard_update_clock,
+            html.Div(html.H2(
+                "Network"), className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom"),
+            network_chart.layout,
+            html.Div(html.H2("Map"), id="geo",
+                     className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom"),
+            map_chart.layout
+        ]
+
+        ############################
+
+        vitals_update_clock = dcc.Interval(
+            id='vitals_update_clock',
+            interval=config.get_config().application.components.vitals_update_interval)
+
+        layouts["vitals"] = [
+            vitals_update_clock,
+            html.Div(html.H2(
+                "Vitals"), className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom"),
+            html.Table([
+                html.Div(battery_status.layout, style={
+                         "display": "inline-block"}),
+                html.Div(ram_status.layout, style={
+                    "display": "inline-block"}),
+                html.Div(cpu_status.layout, style={
+                    "display": "inline-block"}),
+                html.Div(disk_status.layout, style={
+                    "display": "inline-block"}), ])
+        ]
+
+        main = html.Main(
+            children=[],
+            id='main_section',
             className="col-md-9 ml-sm-auto col-lg-10 px-md-4",
             role="main"
         )
@@ -324,6 +445,7 @@ class Application(Thread):
         #
         layout = html.Div(
             [
+                dcc.Location(id='url', refresh=False),
                 navbar,
                 html.Div(
                     [
@@ -382,11 +504,59 @@ class Application(Thread):
 ##########################################################################
 
 
-@dash.callback([Output('network', 'elements'),
-                Output('geolocation', 'data')], Input('charts_update_clock', 'n_intervals'))
-def charts_update(n_intervals):
+@dash.callback([Output('main_section', 'children'),
+                Output('nav_links', 'children')],
+               [Input('url', 'pathname')])
+def display_page(pathname):
+    """Send the current layout"""
+    global current_layout
+    if pathname == "/":
+        current_layout = default_layout
+    else:
+        current_layout = pathname[1:]
+    return (layouts[current_layout], get_navlinks())
+
+
+@dash.callback([Output('battery_status', 'figure'),
+                Output('ram_status', 'figure'),
+                Output('cpu_status', 'figure'),
+                Output('disk_status', 'figure')],
+               [Input('vitals_update_clock', 'n_intervals')])
+def vitals_update(n_intervals):
     """Refresh the charts"""
-    return network_chart.get_data(), map_chart.get_data()
+    # https://psutil.readthedocs.io/en/latest/#processes
+    battery = psutil.sensors_battery()
+    battery_status.value = battery.percent
+    if battery.power_plugged:
+        battery_status.subtitle = "Plugged"
+    else:
+        battery_status.subtitle = "Unplugged"
+        # Right after unpluging, the battery life time is false (too long)
+        if battery.secsleft < (24*60*60):
+            battery_status.subtitle += f' - {datetime.timedelta(seconds=battery.secsleft)} left'
+
+    current_process = psutil.Process()
+    ram_status.subtitle = f'Cartographe: {bytes2human(current_process.memory_info().rss)} ({int(current_process.memory_percent())}%)'
+    ram_status.value = int(psutil.virtual_memory().percent)
+
+    cpu_status.subtitle = f'Cartographe: {int(current_process.cpu_percent(interval=None))}%'
+    cpu_status.value = int(psutil.cpu_percent(interval=None))
+
+    project_abs_path = str(pathlib.Path(__file__).parent.absolute())
+    disk_stats = psutil.disk_usage(project_abs_path)
+    app_disk_used = sum(f.stat().st_size for f in pathlib.Path(
+        '.').glob('**/*') if f.is_file())
+    disk_status.subtitle = f"Cartographe: {bytes2human(app_disk_used)} ({int(app_disk_used * 100 / disk_stats.total)}%)"
+    disk_status.value = int(disk_stats.percent)
+
+    return (battery_status.get_layout(), ram_status.get_layout(), cpu_status.get_layout(), disk_status.get_layout())
+
+
+@dash.callback([Output('network', 'elements'),
+                Output('geolocation', 'data')], Input('dashboard_update_clock', 'n_intervals'))
+def dashboard_update(n_intervals):
+    """Refresh the charts"""
+    return (network_chart.get_data(), map_chart.get_data())
 
 
 @dash.callback([Output("map_info", "header"),
@@ -410,6 +580,6 @@ def geolocation_hover(feature):
         info = (html.P(info,
                        className="mb-0",
                        style={"padding": ".75rem"}))
-        return title, info
+        return (title, info)
     else:
-        return "Survole un point pour obtenir des informations", [""]
+        return ("Survole un point pour obtenir des informations", [""])
